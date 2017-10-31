c7ce65780e777404e76c5f035dcb34ab21e01430
xiahaihu <xiahaihu@meizu.com>
2017-10-31 21:18:40 +0800

    init project

---
+++ src/main/java/com/shearf/cloud/apps/captcha/pub/CaptchaPublicApplication.java
@@ -0,0 +1,15 @@
+package com.shearf.cloud.apps.captcha.pub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiahaihu2009@gmail.com
+ */
+@SpringBootApplication
+public class CaptchaPublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptchaPublicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shearf/cloud/apps/captcha/pub/CaptchaPublicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shearf.cloud.apps.captcha.pub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaptchaPublicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
