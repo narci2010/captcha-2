48b595d0d2fe6ed597da29e6825e6c5b395a5450
xiahaihu <xiahaihu@meizu.com>
2017-12-05 15:47:08 +0800

    优化docker运行，指定时区

--- src/main/docker/runboot.sh
+++ src/main/docker/runboot.sh
@@ -6,4 +6,7 @@ java -Ddatabase.ip=${DATABASE_IP} \
     -Djava.security.egd=file:/dev/./urandom \
     -Dspring.redis.port=${REDIS_PORT} \
     -Dspring.redis.host=${REDIS_HOST} \
+    -Dfile.encoding=UTF-8 \
+    -Duser.timezone=Asia/Shanghai \
+    -server \
     -jar /app/app.jar
